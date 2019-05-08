d85e17f3ae9ff18b867a8029dfa86415fa6b4cfb
ftraxler-tgm <ftraxler@student.tgm.ac.at>
2019-05-08 11:00:50 +0200

    Passwords changed

--- src/main/resources/testdb/data.sql
+++ src/main/resources/testdb/data.sql
@@ -1,6 +1,6 @@
 
-insert into T_User (FNAME,LNAME,ROLE,PASSWORD) values ('ftraxler','test','test','1234');
-insert into T_User (FNAME,LNAME,ROLE,PASSWORD) values ('komar','test','test','1234');
-insert into T_User (FNAME,LNAME,ROLE,PASSWORD) values ('dkostroun','test','test','1234');
+insert into T_User (FNAME,LNAME,ROLE,PASSWORD) values ('ftraxler','test','test','ftraxlerps');
+insert into T_User (FNAME,LNAME,ROLE,PASSWORD) values ('komar','test','test','komarps');
+insert into T_User (FNAME,LNAME,ROLE,PASSWORD) values ('dkostroun','test','test','dkostrounps');
 
 
